9618093a8e4e9b17af9afecbc0a659a7237e047f
lancy.fernandes <lancy@LancyNF-InteliMac.local>
2013-07-12 15:19:30 +0530

    Intial Commit

---
+++ MyNewProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyNewProject
+//
+//  Created by lancy.fernandes on 12/07/13.
+//  Copyright (c) 2013 Lancy Norbert Fernandes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyNewProjectTests/MyNewProjectTests.m
@@ -0,0 +1,36 @@
+//
+//  MyNewProjectTests.m
+//  MyNewProjectTests
+//
+//  Created by lancy.fernandes on 12/07/13.
+//  Copyright (c) 2013 Lancy Norbert Fernandes. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyNewProjectTests : XCTestCase
+
+@end
+
+@implementation MyNewProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
